51dd1dc338bd4140a063ae2c2138dddbfbb492eb
JayV-1 <fbarrosjoao@gmail.com>
2024-04-16 21:38:49 -0300

    Correção e Structs
    Criação dos structs iniciais

--- main.c
+++ main.c
@@ -1,21 +1,40 @@
-
 #include <stdio.h>
+#include <string.h> 
+//criar hora que 
+struct hora {
+    int horas,
+    minutos;
+};
+
+//struct para a data que a compra foi feita
+struct data {
+    int day, 
+    month, 
+    year;
+    
+    struct hora hora_compra;
+};
 
 //struct para os produtos que serão vendidos
-struct produto(){
+struct produto {
     int id;
     char nome[15];
     float preço;
     int popularidade;
-}
+};
 
-struct compra(){
-    struct produto;
-}
+//struct para as compras dos produtos
+struct compra {
+    struct produto produto_compra;
+    struct data data_compra;
+    
+};
 
-int main()
-{
+//
+int main() {
     int lista_produtos[5];
+    
+    struct compra compras;
 
     return 0;
 }
